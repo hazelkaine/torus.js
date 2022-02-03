6caa6d64d1a5557efbafbd8f4f3ce92771b18ede
Hazel Kaine <94728019+hazelkaine@users.noreply.github.com>
2022-02-02 21:38:03 -0700

    Create Xvectortorus.js

---
+++ Xvectortorus.js
@@ -0,0 +1,29 @@
+let angle =0;
+
+function setup() {
+  createCanvas(500, 400, WEBGL);
+}
+
+function draw() {
+  background(175);
+  rotateX(angle);
+  rotateY(angle * .3);
+  rotateZ(angle * 1.2);
+  
+  pointLight(0,0,255, -200, -200,0);
+  pointLight(255,0,0, 200, 200,0);
+  //translate(mouseX -width/1.5, mouseY - height/1.5);
+  fill( 200,0,255);
+  noStroke();
+ ambientMaterial(255);
+  torus(100,10,100);
+  //torus(200,10,200);
+  box (10, 100,10);
+  //box(100,10,10);
+  box(10,10,100);
+  //forwardshash out boxes if you want a differnt X postion
+  
+  
+  
+  angle +=.03;
+}
